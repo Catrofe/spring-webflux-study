7417e8cc5e4152000790ad646d82425e7a1d07a7
Catrofe <lord.rico10@gmail.com>
2025-12-14 11:14:14 -0300

    TESTS: Adicionando testes de integração para sistema de conversão.

--- src/test/kotlin/com/example/springwebfluxstudy/web/CurrencyControllerIntegrationTest.kt
+++ src/test/kotlin/com/example/springwebfluxstudy/web/CurrencyControllerIntegrationTest.kt
@@ -3,6 +3,7 @@ package com.example.springwebfluxstudy.web
 import com.example.springwebfluxstudy.adapter.`in`.web.dto.CurrencyConversionRequest
 import com.example.springwebfluxstudy.adapter.`in`.web.dto.CurrencyConversionResponse
 import com.example.springwebfluxstudy.adapter.out.web.exchangeRate.dto.ExchangeRateConverterResponse
+import com.example.springwebfluxstudy.domain.model.CachedRate
 import com.example.springwebfluxstudy.domain.model.CurrencyExchangeLog
 import com.example.springwebfluxstudy.domain.port.out.CurrencyExchangeLogRepository
 import com.example.springwebfluxstudy.domain.port.out.ExchangeRateClientPort
@@ -77,6 +78,83 @@ class CurrencyControllerIntegrationTest {
                 val body = response.responseBody
                 assert(body != null)
                 assertEquals(0, body!!.convertedAmount.compareTo(BigDecimal("550.00")))
+            }
+    }
+
+
+    @Test
+    fun `deve encontrar moeda no cache com sucesso`() {
+        val request = CurrencyConversionRequest(
+            from = "USD",
+            to = "BRL",
+            amount = BigDecimal("200.00")
+        )
+
+        val cachedRate = CachedRate(
+            quote = BigDecimal("5.50"),
+            keyPrincipal = "USD"
+        )
+
+        given(rateCachePort.getRate(any(), any()))
+            .willReturn(Mono.just(cachedRate))
+
+        given(exchangeRateClientPort.getExchangeRate(any(), any(), any()))
+            .willReturn(Mono.empty())
+
+        given(currencyExchangeLogRepository.save(any()))
+            .willAnswer { invocation ->
+                Mono.just(invocation.getArgument(0, CurrencyExchangeLog::class.java))
+            }
+
+        webTestClient.post()
+            .uri("/v1/currency")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(request)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(CurrencyConversionResponse::class.java)
+            .consumeWith { response ->
+                val body = response.responseBody
+                assert(body != null)
+                assertEquals(0, body!!.convertedAmount.compareTo(BigDecimal("1100.00")))
+            }
+    }
+
+    @Test
+    fun `deve encontrar moeda no cache invertida com sucesso`() {
+        val request = CurrencyConversionRequest(
+            from = "USD",
+            to = "BRL",
+            amount = BigDecimal("200.00")
+        )
+
+        val cachedRate = CachedRate(
+            quote = BigDecimal("0.20"),
+            keyPrincipal = "BRL"
+        )
+
+        given(rateCachePort.getRate(any(), any()))
+            .willReturn(Mono.just(cachedRate))
+
+        given(exchangeRateClientPort.getExchangeRate(any(), any(), any()))
+            .willReturn(Mono.empty())
+
+        given(currencyExchangeLogRepository.save(any()))
+            .willAnswer { invocation ->
+                Mono.just(invocation.getArgument(0, CurrencyExchangeLog::class.java))
+            }
+
+        webTestClient.post()
+            .uri("/v1/currency")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(request)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(CurrencyConversionResponse::class.java)
+            .consumeWith { response ->
+                val body = response.responseBody
+                assert(body != null)
+                assertEquals(0, body!!.convertedAmount.compareTo(BigDecimal("1000.00")))
             }
     }
 }
